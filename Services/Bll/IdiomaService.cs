8ef8b74f6e80e7e71dc99aab4f58572fc62c5914
Brian Gomez <brian.gomez@intellexis.com.ar>
2025-05-23 12:43:48 -0300

    Clase 9/5 y 16/5

---
+++ Services/Bll/IdiomaService.cs
@@ -0,0 +1,48 @@
+﻿using Services.Dal;
+using Services.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Bll
+{
+    public sealed class IdiomaService
+    {
+        private readonly static IdiomaService _instance = new IdiomaService();
+
+        public static IdiomaService Current
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private IdiomaService()
+        {
+            //Implent here the initialization of your singleton
+        }
+
+
+        public string Traducir(string word)
+        {
+            try
+            {
+                return IdiomaRepository.Current.Traducir(word);
+            }
+            catch (WordNotFoundException ex)
+            {
+                //Podría aplicar una nueva política
+                IdiomaRepository.Current.AgregarDataKey(word);
+                //Esto seguramente vaya a una bitácora
+                Console.WriteLine(ex.Message);
+
+                return word;
+            }
+        }
+
+    }
+
+}
